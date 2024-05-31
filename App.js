a7f371b67dcb152f0732d93ab3bb5be91616fca5
stormymayday <stormymayday7@gmail.com>
2024-05-31 11:09:56 -0700

    Wired up Navigation in App

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import "react-native-gesture-handler";
 
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./src/navigation/Navigation";
+
+// Importing Navigation container
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Hello World</Text>
-            <StatusBar style="auto" />
+            <Navigation />
         </View>
     );
 }
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,7 +1,12 @@
 // 1. Importing the Navigation Container
 import { NavigationContainer } from "@react-navigation/native";
+import Feed from "../screens/Feed";
 
 // 2. Exporting the Main Navigation Container
 export default Navigation = () => {
-    return <NavigationContainer></NavigationContainer>;
+    return (
+        <NavigationContainer>
+            <Feed />
+        </NavigationContainer>
+    );
 };
